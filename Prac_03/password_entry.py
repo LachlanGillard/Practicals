bf8ef770c352577e39bdbca4e490114d4c7a795f
LachlanGillard <lachlan.gillard@my.jcu.edu.au>
2019-08-20 10:13:43 +1000

    Refactor password checker to use functions

--- Prac_03/password_entry.py
+++ Prac_03/password_entry.py
@@ -1,9 +1,27 @@
 """
 Lachlan Gillard
 """
-password = input("Password: ")
-while len(password) < 6:
-    print("Invalid Length")
+
+
+def main():
+    password = get_password()
+    asteriks = covert_password_asterisks(password)
+    print(asteriks)
+
+
+def covert_password_asterisks(password):
+    asterisks = '*' * len(password)
+    return asterisks
+
+
+def get_password():
     password = input("Password: ")
-asteriks = '*' * len(password)
-print(asteriks)
+    while len(password) < 6:
+        print("Invalid Length")
+        password = input("Password: ")
+    return password
+
+
+main()
+
+
